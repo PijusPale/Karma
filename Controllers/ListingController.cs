d3acc78be10838e6249b39f914ba3de8aacb3a3f
SimonasKalpokas <simonas.kalpokas@gmail.com>
2021-09-21 12:07:11 +0300

    Move main classes to Models folder

--- Controllers/ListingController.cs
+++ Controllers/ListingController.cs
@@ -6,6 +6,7 @@ using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Karma.Models;
 
 namespace Karma.Controllers
 {
--- Models/Comment.cs
+++ Models/Comment.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections.Generic;
 
-namespace Karma
+namespace Karma.Models
 {
 	public class Comment
 	{
