03c3663caf9da9275dc1187c71d33e07a4433bb5
Kalpokas_Simonas <simonas.kalpokas@gmail.com>
2021-10-11 17:38:57 +0300

    Add backend for listing update

--- Controllers/ListingController.cs
+++ Controllers/ListingController.cs
@@ -26,11 +26,12 @@ namespace Karma.Controllers
 
         [HttpPost]
         [Authorize]
-        public ActionResult Post(Listing listing) {
+        public ActionResult Post(Listing listing)
+        {
             var random = new Random();
             listing.Id = random.Next(9999).ToString(); // temp fix for id generation, later this should be assigned in DB.
             listing.DatePublished = DateTime.UtcNow; //temp fix for curr date with form submit
-            
+
             string userId = User.FindFirst(ClaimTypes.Name)?.Value;
             listing.OwnerId = userId;
             _listingRepository.AddListing(listing);
@@ -44,15 +45,37 @@ namespace Karma.Controllers
         }
 
         [HttpGet("{id}")]
-        public Listing GetListingById(string id) 
+        public Listing GetListingById(string id)
         {
             return _listingRepository.GetListingById(id);
         }
 
         [HttpDelete("{id}")]
         [Authorize]
-        public void DeleteListing(string id) {
+        public IActionResult DeleteListing(string id)
+        {
+            string userId = User.FindFirst(ClaimTypes.Name)?.Value;
+            var listing = _listingRepository.GetListingById(id);
+            if (listing.OwnerId != userId)
+                return Unauthorized();
             _listingRepository.DeleteListingById(id);
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        [Authorize]
+        public IActionResult UpdateListing(string id, [FromBody] Listing listing)
+        {
+
+            var old = _listingRepository.GetListingById(listing.Id);
+            if (old == null) return NotFound();
+
+            string userId = User.FindFirst(ClaimTypes.Name)?.Value;
+            if (old.OwnerId != userId) return Unauthorized();
+
+
+            _listingRepository.UpdateListing(listing);
+            return Ok();
         }
     }
 }
--- Repositories/ListingRepository.cs
+++ Repositories/ListingRepository.cs
@@ -28,19 +28,27 @@ namespace Karma.Repositories
         }
 
         public Listing GetListingById(string id)
-        {            
+        {
             List<Listing> listings = GetAllListings().ToList();
             return listings.FirstOrDefault(x => x.Id == id);
         }
 
-        public void DeleteListingById(string id) 
+        public void DeleteListingById(string id)
         {
             List<Listing> listings = GetAllListings().ToList();
             listings.Remove(listings.Find(x => x.Id == id));
             writeListingsToFile(listings);
         }
 
-        private void writeListingsToFile(List<Listing> listings) {
+        public void UpdateListing(Listing listing)
+        {
+            List<Listing> listings = GetAllListings().ToList();
+            listings[listings.FindIndex(l => l.Id == listing.Id)] = listing;
+            writeListingsToFile(listings);
+        }
+
+        private void writeListingsToFile(List<Listing> listings)
+        {
             var jsonOptions = new JsonSerializerOptions() { WriteIndented = true };
             string jsonString = JsonSerializer.Serialize(listings, jsonOptions);
             System.IO.File.WriteAllText(_filePath, jsonString);
