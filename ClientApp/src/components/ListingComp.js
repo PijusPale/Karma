4aa65962ca91fa880bbcf91a44b32b8433a35113
Brazauskas Deividas <deividas.brazauskas@mif.stud.vu.lt>
2021-10-13 09:30:53 +0300

    Implement Item Requests

--- ClientApp/src/components/ListingComp.js
+++ ClientApp/src/components/ListingComp.js
@@ -6,8 +6,38 @@ import AddListing from './AddListing';
 export const ListingComp = (props) => {
   const { user } = useContext(UserContext);
   const [update, setUpdate] = useState(false);
+  const [show, setShow] = useState(false);
+  const [requestResponse, setResponse] = useState(0);
 
   const toggleModal = () => setUpdate(!update);
+
+  const CustomAlert = () => {
+    let variant, description;
+
+    if(requestResponse === 200)
+    {
+      variant = 'alert alert-success';
+      description = 'You have successfully requested the item.';
+    }
+    if(requestResponse === 403)
+    {
+      variant = 'alert alert-danger';
+      description = 'You cannot request your own item.';
+    }
+    if(requestResponse === 409)
+    {
+      variant = 'alert alert-warning';
+      description = 'You have already requested this item.';
+    }
+
+    if(show) {
+      return(
+        <div class={variant} role="alert">
+          {description}
+        </div> 
+      );
+    }   
+  }
 
   const onDelete = async () => {
     const res = await fetch(`listing/${props.id}`, {
@@ -18,6 +48,25 @@ export const ListingComp = (props) => {
       }
     });
     res.ok && props.refresh();
+  };
+
+  const onRequest = async () => {
+    const res = await fetch(`listing/request/${props.id}`, {
+      method: 'GET',
+      headers: {
+        'Authorization': `Bearer ${localStorage.getItem('token')}`
+      }
+    });
+    if(res.status === 200)
+      setResponse(200);
+    if(res.status === 403)
+      setResponse(403);
+    if(res.status === 409)
+      setResponse(409);
+    setShow(true);
+    //console.log(requestResponse);
+    //console.log(res.status);
+    //console.log(show);
   };
 
   return (
@@ -37,12 +86,17 @@ export const ListingComp = (props) => {
               <Button onClick={onDelete}>Delete</Button>
               <Button onClick={() => setUpdate(true)}>Update</Button>
             </>}
+          {user && user.id !== props.ownerId && !requestResponse &&
+            <>
+              <Button onClick={onRequest}>Request Item</Button>
+            </>}
+          {CustomAlert()}
         </div>
       </div>
       {update && <Modal isOpen={update} toggle={toggleModal}>
         <ModalHeader><Label>Update</Label></ModalHeader>
         <ModalBody><AddListing {...props} update={true}
-          afterSubmit={() => { toggleModal(); props.refresh(); }}/></ModalBody>
+          afterSubmit={() => { toggleModal(); props.refresh(); }} /></ModalBody>
       </Modal>}
     </div>);
 };
