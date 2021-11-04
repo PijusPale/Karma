39a12fcd63fb9479fbabcc30ea1bc40efc3ebceb
Juste <justebarto@gmail.com>
2021-11-04 21:09:50 +0200

    add nofitication template

---
+++ ClientApp/src/components/Nofitication.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Nofitication = () => {
+
+	return (
+        <div id="nofitication" class="nofitication">
+            <p>The item has been requested</p>
+        </div>
+	)
+}
+export default Nofitication
+
+/* <div id="close" class="nofitication__close" onClick={() => remove(this)}>
+                <p>X</p>
+            </div> */
