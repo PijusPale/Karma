5d730a4bfc4ceed50c930a412a9cd8d7b9d49f51
JusteBarto <justebarto@gmail.com>
2021-11-10 20:14:21 +0200

    minor fixes

--- ClientApp/src/components/ListingsComp.js
+++ ClientApp/src/components/ListingsComp.js
@@ -46,7 +46,6 @@ export const ListingsComp = () => {
         }
     }
 
-
     const indexOfLastListing = currentPage * listingsPerPage;
     const indexOfFirstListing = indexOfLastListing - listingsPerPage;
 
