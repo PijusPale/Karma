b5e030c2f45d549a372aa1e16c820ea4f9f34439
Brazauskas Deividas <deividas.brazauskas@mif.stud.vu.lt>
2021-12-02 17:29:26 +0200

    remaster chat server-side

--- Services/ChatHub.cs
+++ Services/ChatHub.cs
@@ -15,10 +15,34 @@ namespace Karma.Services
     public class ChatHub : Hub
     {
         private readonly IMessageService _messageService;
+
+        private readonly IUserService _userService;
+
+        private static List<int> usersOnline = new List<int>();
         
-        public ChatHub(IMessageService messageService)
+        public ChatHub(IMessageService messageService, IUserService userService)
         {
             _messageService = messageService;
+            _userService = userService;
+        }
+
+        public async override Task OnConnectedAsync()
+        {
+            await base.OnConnectedAsync();
+            if(Context.UserIdentifier != null)
+                usersOnline.Add(int.Parse(Context.UserIdentifier));
+        }
+
+        public async override Task OnDisconnectedAsync(Exception ex)
+        {
+            await base.OnDisconnectedAsync(ex);
+            if(Context.UserIdentifier != null){
+                var userId = int.Parse(Context.UserIdentifier);
+                usersOnline.Remove(userId);
+                var user = _userService.GetUserById(userId);
+                user.LastActive = DateTime.UtcNow;
+                _userService.Update(user);
+            }
         }
     
         public Task SendMessageAsync(string user, string message)
@@ -31,12 +55,23 @@ namespace Karma.Services
             return Clients.Caller.SendAsync("ReceiveMessageToCaller", user, message);
         }
 
+        public Task LastActiveRequest(int userId)
+        {
+            var user = _userService.GetUserById(userId);
+            return Clients.Caller.SendAsync("GetLastActive", user.LastActive);
+        }
+
         public Task SendGroupMessageAsync(string groupId, int user, string message)
         {   
             _messageService.AddMessage(message, Context.UserIdentifier, groupId);
             _messageService.SaveMessages(groupId);
 
             return Clients.Group(groupId).SendAsync("ReceiveGroupMessage", user, message);
+        }
+
+        public Task SendTypingNotificationAsync(string groupId, int userId, bool isTyping)
+        {
+            return Clients.Group(groupId).SendAsync("ReceiveTypingNotification", userId, isTyping);
         }
 
         public async Task AddToGroupAsync(string groupId)
@@ -46,8 +81,6 @@ namespace Karma.Services
 
         public async Task RemoveFromGroupAsync(string groupId)
         {
-            _messageService.SaveMessages(groupId);
-
             await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupId);
         }
     }
--- Services/MessageService.cs
+++ Services/MessageService.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Text;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using System.Linq;
 using Microsoft.AspNetCore.SignalR;
 using Karma.Models;
@@ -14,18 +16,26 @@ namespace Karma.Services
         IEnumerable<Message> GetByLimit(string groupId, int limit, string lastMessageId);
         void AddMessage(string content, string userId, string groupId);
         void SaveMessages(string groupId);
+        void CreateConversation(int userOneId, int userTwoId, int listingId);
         IEnumerable<ConversationDto> GetConversations(int userId); 
+        Conversation GetConversation(string groupId);
     }
 
     public class MessageService : IMessageService
     {
         private readonly IMessageRepository _messageRepository;
 
+        private readonly IListingRepository _listingRepository;
+
+        private readonly IUserService _userService;
+
         private List<Message> _messages;
 
-        public MessageService(IMessageRepository messageRepository)
+        public MessageService(IMessageRepository messageRepository, IListingRepository listingRepository, IUserService userService)
         {
             _messageRepository = messageRepository;
+            _listingRepository = listingRepository;
+            _userService = userService;
             _messages = new List<Message>();
         }
 
@@ -46,9 +56,7 @@ namespace Karma.Services
 
         public void AddMessage(string content, string userId, string groupId)
         {
-            var random = new Random();
-            string messageId = random.Next(9999).ToString();
-            var message = new Message(id: int.Parse(messageId), content: content, fromId: int.Parse(userId), groupId: groupId, dateSent: DateTime.UtcNow, status: 0);
+            var message = new Message(content: content, fromId: int.Parse(userId), groupId: groupId, dateSent: DateTime.UtcNow, status: 0);
             _messages.Add(message);  
         }
 
@@ -57,9 +65,68 @@ namespace Karma.Services
             _messageRepository.Add(_messages, groupId);
         }
 
+        public void CreateConversation(int userOneId, int userTwoId, int listingId)
+        {
+            var groupId = Guid.NewGuid().ToString("N");
+            _messageRepository.CreateConversation(userOneId, userTwoId, listingId, groupId);
+        }
+
         public IEnumerable<ConversationDto> GetConversations(int userId)
         {
-            return Enumerable.Empty<ConversationDto>();
+            var conversations = _messageRepository.GetConversations(userId);
+            var listings = _listingRepository.GetListingsById(
+                (from convo in conversations
+                select convo.ListingId)
+                .ToList());
+            var messages = new List<Message>();
+            foreach (var convo in conversations)
+            {
+                var message = _messageRepository.GetByLimit(convo.GroupId, 1);
+                messages.AddRange(message);
+            }
+            var query = from c in conversations
+                        join l in listings
+                        on c.ListingId equals l.Id
+                        select new  {
+                                    ListingName = l.Name,
+                                    ListingImg = l.ImagePath,
+                                    GroupId = c.GroupId,
+                                    ListingId = l.Id,
+                                    UserOneId = c.UserOneId,
+                                    UserTwoId = c.UserTwoId,
+                                    };
+            var result = (from q in query
+                        select new ConversationDto {
+                        ListingName = q.ListingName,
+                        LastSender = "",
+                        LastMessage = "",
+                        UserOneId = q.UserOneId,
+                        UserTwoId = q.UserTwoId,
+                        UserOneName = _userService.GetUserById(q.UserOneId).FirstName,
+                        UserTwoName = _userService.GetUserById(q.UserTwoId).FirstName,
+                        ListingImg = q.ListingImg,
+                        GroupId = q.GroupId,
+                        ListingId = q.ListingId
+                        }).ToList();
+            
+            foreach (var item in result)
+            {
+                var msg = messages.FirstOrDefault(x => x.GroupId == item.GroupId);
+                if(msg != null)
+                {
+                    var user = _userService.GetUserById(msg.FromId);
+                    item.LastSender = user.FirstName;
+                    item.LastMessage = msg.Content;
+                    item.DateSent = msg.DateSent;
+                }
+
+            }
+            return result.OrderByDescending(c => c.DateSent);     
+        }
+
+        public Conversation GetConversation(string groupId)
+        {
+            return _messageRepository.GetConversation(groupId);
         }
     }
 }
