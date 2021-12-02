09be4f0992829261a31f602098c09f9316c8d93c
Brazauskas Deividas <deividas.brazauskas@mif.stud.vu.lt>
2021-12-02 18:53:42 +0200

    typo v2 - fix import name

--- Repositories/DbMessageRepository.cs
+++ Repositories/DbMessageRepository.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Karma.data.messages;
+using Karma.Database;
 using Karma.Models;
 using Microsoft.EntityFrameworkCore;
 
