e1c0d0765e4651c06a50cabac026b948f02ab933
Kalpokas_Simonas <simonas.kalpokas@gmail.com>
2021-12-06 20:06:13 +0200

    Add a flower and grass to canvas

--- ClientApp/src/garden/Block.js
+++ ClientApp/src/garden/Block.js
@@ -1,23 +1,47 @@
 import React, { useRef, useState } from 'react';
+import Tree from './tree';
+import Grass from './grass';
+import Flower from './flower';
 
 export const Block = (props) => {
-    // This reference gives us direct access to the THREE.Mesh object
-    const ref = useRef()
-    // Hold state for hovered and clicked events
-    const [hovered, hover] = useState(false)
-    const [clicked, click] = useState(false)
-    // Subscribe this component to the render-loop, rotate the mesh every frame
-    // Return the view, these are regular Threejs elements expressed in JSX
-    return (
-      <mesh
-        {...props}
-        ref={ref}
-        scale={clicked ? 1.5 : 1}
-        onClick={(e) => {e.stopPropagation(); click(!clicked)}}
-        onPointerOver={(e) => {e.stopPropagation(); hover(true)}}
-        onPointerOut={(e) =>{e.stopPropagation(); hover(false)}}>
-        <boxGeometry args={[1, 1, 1]} />
-        <meshStandardMaterial color={hovered ? 'hotpink' : '#567d46'} />
-      </mesh>
-    )
+  // This reference gives us direct access to the THREE.Mesh object
+  const group = useRef();
+  const [hovered, hover] = useState(false);
+  const [clicked, click] = useState(false);
+
+  const onClick = (e) => {
+    click(!clicked);
+    //console.log(e.eventObject.position)
+    //e.eventObject.position
   };
+  return (
+    <group ref={group} {...props} dispose={null}>
+      <mesh
+        scale={1}
+        onClick={(e) => { e.stopPropagation(); onClick(e) }}
+        onPointerOver={(e) => {  e.stopPropagation(); hover(true) }}
+        onPointerOut={(e) => { e.stopPropagation(); hover(false) }}>
+        <boxGeometry args={[1, 0.2, 1]} />
+        <meshStandardMaterial color={hovered ? 'hotpink' : '#63a844'} />
+      </mesh>
+      <mesh position={[0,-0.5, 0]}>
+        <boxGeometry args={[1, 0.8, 1]} />
+        <meshStandardMaterial color='#605139' />
+      </mesh>
+      {((props.position[0] % 3 === 0 && props.position[2] % 3 === 0) ||
+        (Math.abs(props.position[0]) % 2 === 1 && Math.abs(props.position[2]) % 2 === 1))
+         && <Grass position={[0, 0.1, 0]} scale={1}/>}
+      {props.plant === 'tree' && clicked && <Tree position={[0, 1, 0]}/>}
+      {props.plant === 'flower' && clicked && <Flower position={[0, 0, 0]}/>}
+    </group>
+  );
+};
+
+/*Make text:
+import { Text } from "@react-three/drei";
+<Text scale={[10, 10, 10]}
+        color="black" // default
+        anchorX="center" // default
+        anchorY="middle" // default
+        >Labas</Text>
+        */
--- ClientApp/src/garden/garden.js
+++ ClientApp/src/garden/garden.js
@@ -3,34 +3,30 @@ import { Canvas } from '@react-three/fiber'
 import Tree from './tree'
 import { OrbitControls } from '@react-three/drei'
 import { Block } from './Block';
+import Flower from './flower';
 
 const generateGridCoords = (size) => {
     let arr = [];
     const helper = Math.floor(size/2);
     for (let i = -1*helper; i < helper+1; i++) {
         for (let j =-1*helper; j < helper + 1; j++) {
-            arr.push([i, -3, j])
+            arr.push([i, -1, j])
         }
     }
     return arr;
 };
-const gridSize = 6; //should be odd
+const gridSize = 9; //should be odd
 let groundCoords = generateGridCoords(gridSize);
 
 export const GardenComp = () => {
 
     return (<Canvas style={{ height: "400px" }}>
-        <OrbitControls position0={[0, 5, -100]}/>
+        <OrbitControls/>
         <ambientLight intensity={0.5} />
-        <spotLight position={[10, 15, 10]} angle={0.3} />
-        <Suspense fallback={null}>
-            <Tree scale={26} position={[0, -1, 0]} />
-        </Suspense>
+        <spotLight intensity={0.3} position={[-10, 15, 10]} angle={0.3} />
         {groundCoords.map(pos => 
-            <Block position={pos} />
+            <Block position={pos} plant='tree' />
         )}
-        {/* <Block position={[0, -3, 0]} /> */}
-        {/* <Environment preset="sunset" background /> */}
-        {/* <primitive object={dae.scene} dispose={null} /> */}
+        <Flower position={[0, -1, 0]}/>
     </Canvas>);
 }
